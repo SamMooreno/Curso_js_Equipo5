6d51ed4e98e8fc073f2e3e5796542b8ac2e06277
marian <nyx198451728@gmail.com>
2025-09-08 23:49:30 +0200

    actividad 2

--- cl6Suma.js
+++ cl6Suma.js
@@ -1,3 +1,11 @@
-//sumar dos numeros en un console.log
 
-console.log(numero1 + numero2);
+                let num = prompt("Escribe un numero mayor a 10: ");
+                if (num >= 10){
+                    let sucesion = [];
+                    for (let i = num; i>= 0; i--){
+                        sucesion.push(i)
+                    }
+                    alert(sucesion);
+                }else{
+                    alert("Error. El numero que escogio es menor a 10");
+                }
