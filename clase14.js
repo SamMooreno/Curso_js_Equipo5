3714904eafe212cf0d56f7cb8fd5f0afcf8dcedc
marian <nyx198451728@gmail.com>
2025-09-25 01:35:40 +0200

    lista de compras

---
+++ clase14.js
@@ -0,0 +1,6 @@
+let arr = ["chocolate","manzana","pan"]
+
+let opciones = prompt (
+    "Selecciona cual quieres 1- Ver lista 2- agregar un elemento a la  3- invertir la lista 4-eliminar un producto de la lista"
+)
+console.log(opciones)
